e1370d81055acb5c49b036aa1cc800cba7f7f358
unknown <jmaldonadom1989@gmail.com>
2016-05-14 15:59:05 -0600

    Contact Message field added and response returned upon reciving post method

---
+++ AutomaticTellerMachine/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AutomaticTellerMachine
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "Serial Number",
+               url: "Serial",
+               defaults: new { controller = "Home", action = "Serial"}
+           );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AutomaticTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomaticTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET /Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET /Home/About
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.TheMessage = "Having trouble?, Send us a message";
+
+            return View();
+        }
+
+        public ActionResult Foo() {
+
+            return View("About");
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message) 
+        {
+            //TODO: Send message to HQ
+            ViewBag.TheMessage = "Thanks we got your message";
+
+            return View();
+        }
+
+
+        public ActionResult Serial(string LetterCase) {
+
+            var SerialNumber = "ASPNETMVCATM1";
+            if (LetterCase == "lower")
+            {
+                return Content (SerialNumber.ToLower());
+            }
+            else {
+                return Json(new {name="Serial", value=SerialNumber }, JsonRequestBehavior.AllowGet);
+            }
+            
+
+        }
+    }
+}
